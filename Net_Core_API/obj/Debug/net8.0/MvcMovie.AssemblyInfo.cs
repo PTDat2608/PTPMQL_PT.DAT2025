467391459c52e40741750b9d91dcc44e0ee48f7c
PTDat2608 <ptdat26082k3@gmail.com>
2025-02-14 16:48:16 +0700

    Test

--- Net_Core_API/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ Net_Core_API/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e630dbaf0f47751463dc146a3f33ec5a7d7a4a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
